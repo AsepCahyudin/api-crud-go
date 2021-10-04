b166b1c3ac32e874d1910ec413eb19a225299f2d
asepCahyudin <asep.cahyudin92@gmail.com>
2021-10-04 14:04:39 +0700

    push master update

--- main.go
+++ main.go
@@ -3,10 +3,19 @@ package main
 import {
 	"fmt"
 	"github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm/dialects/mysql"
 }
 
-develop test
+var db gorm.db
+var err error
 
 func main(){
-	fmt.Println("Hallo kita buat crud golang")
+	// fmt.Println("Hallo kita buat crud golang")
+	db, err = gorm.Open("mysql", "root:@/go_rest_api_crud?charset=utf8&parseTime=True")
+
+	if err != nil {
+		log.Println("Connection failed", err)
+	} else {
+		log.Println("Connection established")
+	}
 }
