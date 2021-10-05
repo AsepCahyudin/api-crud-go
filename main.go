434d514184dadf30f24fa5fb657a09b247e9cd82
asepCahyudin <asep.cahyudin92@gmail.com>
2021-10-05 11:21:15 +0700

    test struct

--- main.go
+++ main.go
@@ -1,21 +1,33 @@
 package main
 
-import {
+import (
+	// "log"
+	// "net/http"
+	// "golang/api-crud-go/config"
+
+
 	"fmt"
-	"github.com/jinzhu/gorm"
-	"github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type Profil struct {
+	ID      int
+	Name    string
+	Age     int
+	Address string
 }
 
-var db gorm.db
-var err error
+func main() {
+	var profil Profil
 
-func main(){
-	// fmt.Println("Hallo kita buat crud golang")
-	db, err = gorm.Open("mysql", "root:@/go_rest_api_crud?charset=utf8&parseTime=True")
+	profil.ID = 1
+	profil.Name = "Didik Prabowo"
+	profil.Age = 20
+	profil.Address = "Wuryantoto, Wonogiri"
 
-	if err != nil {
-		log.Println("Connection failed", err)
-	} else {
-		log.Println("Connection established")
-	}
+	fmt.Println(profil)
+	fmt.Println("===========================")
+	fmt.Printf("Nama \t: %v\n", profil.Name)
+	fmt.Printf("Umur \t: %d\n", profil.Age)
+	fmt.Printf("Alamat \t: %v\n", profil.Address)
+	fmt.Println("===========================")
 }
