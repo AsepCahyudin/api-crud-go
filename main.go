cc69558cc1cf02b522e47356294df69432e531af
asepCahyudin <asep.cahyudin92@gmail.com>
2021-10-06 20:10:34 +0700

    read data

--- main.go
+++ main.go
@@ -4,32 +4,44 @@ import (
 	// "fmt"
 	"log"
 	"net/http"
-	// "time"
-	"api-crud-go/config"
+	// "api-crud-go/config"
+	"api-crud-go/mahasiswa"
+	// "api-crud-go/models"
+	"api-crud-go/utils"
 	"fmt"
+	"context"
 )
 
 
 
 func main() {
-	db, e := config.MySQL()
 
-	if e != nil {
-		log.Fatal(e)
-	}
-
-	eb := db.Ping()
-	if eb != nil {
-		panic(eb.Error()) 
-	}
-
-	fmt.Println("Success")
-
+	http.HandleFunc("/mahasiswa", GetMahasiswa)
 	err := http.ListenAndServe(":7000", nil)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
-
 }
+
+// GetMahasiswa
+func GetMahasiswa(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		ctx, cancel := context.WithCancel(context.Background())
+
+		defer cancel()
+
+		mahasiswas, err := mahasiswa.GetAll(ctx)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		utils.ResponseJSON(w, mahasiswas, http.StatusOK)
+		return
+	}
+
+	http.Error(w, "Tidak di ijinkan", http.StatusNotFound)
+	return
+}
+
